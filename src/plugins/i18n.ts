f84c0c156f3a5078b10669fbbc42764f55787f62
iSmartCoding <ismartcoding@gmail.com>
2023-08-04 17:34:16 +0800

    Add more languages

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,6 +1,10 @@
 import { createI18n } from 'vue-i18n'
 import en_US from '@/locales/en-US'
 import zh_CN from '@/locales/zh-CN'
+import es from '@/locales/es'
+import ja from '@/locales/ja'
+import nl from '@/locales/nl'
+import it from '@/locales/it'
 import { setLocale } from 'yup'
 
 setLocale({
@@ -19,5 +23,9 @@ export default createI18n({
   messages: {
     'en-US': en_US,
     'zh-CN': zh_CN,
+    'es': es,
+    'ja': ja,
+    'nl': nl,
+    'it': it,
   },
 })
