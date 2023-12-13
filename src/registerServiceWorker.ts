ada799cf2089df69aced8bc363ce7e08b771ef07
iSmartCoding <ismartcoding@gmail.com>
2023-12-13 22:59:55 +0800

    Support PWA

---
+++ src/registerServiceWorker.ts
@@ -0,0 +1,14 @@
+if ('serviceWorker' in navigator) {
+  // Register a service worker hosted at the root of the
+  // site using the default scope.
+  navigator.serviceWorker.register('/sw.js').then(
+    (registration) => {
+      console.log('Service worker registration succeeded:', registration)
+    },
+    (error) => {
+      console.error(`Service worker registration failed: ${error}`)
+    }
+  )
+} else {
+  console.error('Service workers are not supported.')
+}
